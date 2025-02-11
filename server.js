570cc1954c773b421818c977d6425b2d4aca8bfd
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-11 21:43:27 +0500

    Refactor server.js for code consistency and improved readability

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-const express = require('express');
-const connectDB = require('./config/DBConnection');
-const userRoutes = require('./routes/userRoute');
-const contactRoutes= require('./routes/contactRoutes')
+const express = require("express");
+const connectDB = require("./config/DBConnection");
+const userRoutes = require("./routes/userRoute");
+const contactRoutes = require("./routes/contactRoutes");
 
-const errorHandler = require('./middleware/errorHandlering')
+const errorHandler = require("./middleware/errorHandlering");
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect DB
-connectDB();
-
-
-
 // Middleware
 app.use(express.json());
-app.use(cookieParser)
+app.use(cookieParser);
 
 // Routes
-app.use('/api', userRoutes);
-app.use('/api/contact', contactRoutes)
-app.use('/api/users', userRoutes)
-app.use(errorHandler)
+app.use("/api", userRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/users", userRoutes);
+app.use(errorHandler);
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  connectDB();
+  console.log(`Server is running on http://localhost:${port}`);
 });
